9d1004f94762ed3309d728346cb58bbbde7f8cf3
jhoncis <jhoncisnerosuarez00@gmail.com>
2022-09-03 17:20:23 +0000

    :fire:Home, jumbo y Navbar listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
+import { Navbar } from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Cards from "./Cards.jsx";
+import Foot from "./foot.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Jumbotron />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-3 col-md-4">
+            <Cards />
+          </div>
+		  <div className="col-lg-3 col-md-4">
+            <Cards />
+          </div>
+		  <div className="col-lg-3 col-md-4">
+            <Cards />
+          </div>
+		  <div className="col-lg-3 col-md-4">
+            <Cards />
+          </div>
+        </div>
+      </div>
+      <Foot />
+    </>
+  );
 };
 
 export default Home;
